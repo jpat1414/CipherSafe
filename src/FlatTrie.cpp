187167141c4c141299504c98354d9aceca587e72
Jenish Patel <jpatel102005@gmail.com>
2025-11-03 16:03:19 -0500

    created constructors and destructors

--- src/FlatTrie.cpp
+++ src/FlatTrie.cpp
@@ -7,10 +7,42 @@ using namespace std;
 
 
 class NodeTrie {
+    public:
+        bool isEnd;
 
+        NodeTrie* children[26];
+
+        NodeTrie() {
+            isEnd = false;
+
+            for (int i = 0; i<26; i++) {
+                children[i] = nullptr;
+            }
+        }
+        ~NodeTrie() {
+            for (int i = 0; i < 26; i++) {
+                delete children[i];
+                children[i] = nullptr;
+
+            }
+        }
     
 };
 
 class Trie {
+    private: 
+        NodeTrie* root;
+        int numElements;
+
+    public:
+
+        Trie() {
+            root = new NodeTrie();
+            numElements = 0;
+
+        }
+
+        
+
 
 };
